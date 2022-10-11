2153b63235dde9980b143bc79d6a9e7a6e4623e5
Divij Sarkale <109907102+divijms07@users.noreply.github.com>
2022-10-11 16:24:24 +0530

    Programs (Java)

---
+++ ArithmeticError.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+class ArithmeticError {
+    public static void main(String[] args) {
+        int a = 0, b = 0, c = 0;
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            try {
+                b = r.nextInt();
+                c = r.nextInt();
+                a = 12 / (b / c);
+            } catch (ArithmeticException e) {
+                System.out.println("Exception: " + e);
+                a = 0;
+            }
+            System.out.println("a: " + a);
+        }
+    }
+}
