b02aba1534f5170f969a9a2d0a82ef87ada69993
Divij Sarkale <109907102+divijms07@users.noreply.github.com>
2022-10-23 14:27:23 +0530

    Add files via upload

---
+++ AbstractDemo.java
@@ -0,0 +1,20 @@
+abstract class A8 {
+    abstract void callme();
+        void callmetoo() {
+            System.out.println("This is a concrete method.");
+        }
+}
+    
+class B8 extends A8 {
+    void callme() {
+        System.out.println("B’s implementation of callme.");
+    }
+}
+
+class AbstractDemo {
+    public static void main(String[] args) {
+        B8 b = new B8();
+        b.callme();
+        b.callmetoo();
+    }
+}
